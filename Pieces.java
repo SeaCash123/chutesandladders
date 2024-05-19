b730f35becdedded6d442711f063cd0c67ffb8ac
SeaCash123 <seanmmurphy2008@gmail.com>
2024-05-19 14:46:08 -0700

    friday may 17

---
+++ Pieces.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Pieces {
+
+    private int x, y;
+
+    public Pieces(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+
+    public void move(int distance){
+        
+
+    }
+
+}
--- Screen.java
+++ Screen.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Font;
 
 
 public class Screen extends JPanel implements ActionListener {
@@ -12,8 +13,9 @@ public class Screen extends JPanel implements ActionListener {
     // Instance variables here
     Square[][] grid = new Square[10][10];
 	private int turn, dice;
-	private JButton roll;
-	
+	private JButton roll, play;
+	private Boolean start;
+	private Font myFont = new Font("Serif", Font.BOLD, 45);
 
 
     // Constructor
@@ -23,13 +25,17 @@ public class Screen extends JPanel implements ActionListener {
 				grid[i][j] = new Square(50 * j + 100, 50 * i + 100);
 			}
 		}
-		
-		turn = 1;
+		start = true;
 		roll = new JButton("ROLL DICE");
+		play = new JButton("START GAME");
 		setLayout(null);
 		roll.setBounds(800,100, 200, 100);
+		play.setBounds(500, 400, 200, 100);
+		add(play);
 		add(roll);
+		roll.setVisible(false);
 		roll.addActionListener(this);
+		play.addActionListener(this);
 		
     }
 
@@ -44,10 +50,19 @@ public class Screen extends JPanel implements ActionListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
 		Color black = new Color(255,255,255);
 		g.drawString("You rolled a " + dice, 940, 250);
-		g.drawString("Current Player: " + turn, 800, 250);
+		g.drawString("Current Player: " + (turn + 1), 800, 250);
 		Color white = new Color(0,0,0);
+		if (start) {
+			roll.setVisible(false);
+			play.setVisible(true);
+		}
+		else {
+			roll.setVisible(true);
+			play.setVisible(false);
+		}
 		
 
 
@@ -59,12 +74,22 @@ public class Screen extends JPanel implements ActionListener {
 			}
 		}
 
+		if (start){
+			g.setColor(new Color(255,255,255));
+			g.fillRect(0,0,1920,1080);
+			g.setColor(new Color(0,0,0));
+			g.setFont(myFont);
+			g.drawString("Snakes and Ladders!", 400, 200);
+		}
+
+
+
 	}
 
 	private int rollDice(){
 		turn++;
-		if (turn>4){
-			turn = 1;
+		if (turn==4){
+			turn = 0;
 		}
 		return (int)(Math.random()*6 + 1);
 	}
@@ -78,6 +103,10 @@ public class Screen extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == roll) {
 			dice = rollDice();
+		}
+
+		if (event.getSource() == play) {
+			start = false;
 		}
 		// You MUST have this line to repaint the screen
 		repaint();
